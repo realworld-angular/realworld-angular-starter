5c63fc0b6cb44bd7ba27eb659cefa24237917baf
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-01 09:25:05 +0200

    feat: add OnPush default strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
 }
