926d317aaf93339bdb73768b66f1452d1a51c67d
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-01 15:15:21 +0200

    feat: add csp nonce rule

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,8 @@ export const environment = {
   apiUrl: '',
   csp: `
     default-src 'self';
-    script-src 'self';
-    style-src 'self';
+    script-src 'self' 'nonce-randomNonceGoesHere';
+    style-src 'self' 'nonce-randomNonceGoesHere';
     font-src 'self';
     img-src 'self' data:;
     object-src 'none';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
   apiUrl: API_URL,
   csp: `
     default-src 'self';
-    script-src 'self';
-    style-src 'self';
+    script-src 'self' 'nonce-randomNonceGoesHere';
+    style-src 'self' 'nonce-randomNonceGoesHere';
     font-src 'self';
     img-src 'self' data:;
     object-src 'none';
