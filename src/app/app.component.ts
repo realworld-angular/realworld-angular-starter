53b146f87f7927d6a4a3ffdc30ff83b10461b713
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-01 19:05:55 +0200

    feat: add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from "./core/layout/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
---
+++ src/app/core/layout/header/header.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    RouterLink,
+    NgOptimizedImage
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+
+}
