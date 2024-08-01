18ba0e013af1fa0233d7dfe2adb74eb13d12602c
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-01 08:59:32 +0200

    refactor: change template to starter

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'realworld-angular-template' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('realworld-angular-template');
+    expect(app.title).toEqual('realworld-angular-starter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, realworld-angular-template');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, realworld-angular-starter');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'realworld-angular-template';
+  title = 'realworld-angular-starter';
 }
