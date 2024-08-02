5dfa0b97c1f53953475d3168a6d5f57e8263a54d
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-03 00:04:07 +0200

    feat(storybook): add first story

---
+++ src/stories/button.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+const meta: Meta = {
+  title: 'Example/Button',
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    label: {
+      control: {type: 'text'},
+    }
+  },
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Primary: Story = {
+  args: {
+    label: "Log In"
+  },
+  render: (args) => ({
+    props: args,
+    template: `<div class="filled-btn">${args['label']}</div>`
+  })
+};
