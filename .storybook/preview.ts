f3da3df3ad8a8fdf100e5a92f7d753eac1f965f5
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-05 19:30:03 +0200

    feat(storybook): add preview dark theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,14 @@
 import type { Preview } from "@storybook/angular";
 import { setCompodocJson } from "@storybook/addon-docs/angular";
+import { themes } from '@storybook/theming';
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark,
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
