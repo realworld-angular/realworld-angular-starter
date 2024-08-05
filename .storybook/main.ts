d1457fb4f880b84ceb689518cf92f91b3423d0a4
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-05 22:01:01 +0200

    feat(storybook): add pseudo-states addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "storybook-addon-pseudo-states",
   ],
   framework: {
     name: "@storybook/angular",
