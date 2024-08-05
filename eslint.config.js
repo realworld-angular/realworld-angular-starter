caea1055067279b6e548468b0a953c28e2d8750d
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-05 20:04:32 +0200

    refactor: update label eslint

--- eslint.config.js
+++ eslint.config.js
@@ -30,6 +30,8 @@ module.exports = tseslint.config(
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
     ],
-    rules: {},
+    rules: {
+      "@angular-eslint/template/label-has-associated-control": "warn",
+    },
   }
 );
