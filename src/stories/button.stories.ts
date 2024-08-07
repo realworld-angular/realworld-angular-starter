2fbb338a8543d783a7cb0f0c860f06a16edce3a0
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-07 18:58:44 +0200

    refactor(storybook): update button stories

--- src/stories/button.stories.ts
+++ src/stories/button.stories.ts
@@ -16,42 +16,86 @@ const meta: Meta = {
 export default meta;
 type Story = StoryObj;
 
+export const Default: Story = {
+  args: {
+    label: 'Log In',
+  },
+  render: (args) => ({
+    props: args,
+    template: `<button class="btn">${args['label']}</button>`,
+  }),
+};
+
+export const DefaultHovered: Story = {
+  ...Default,
+  parameters: {
+    pseudo: {
+      hover: true,
+    },
+  },
+};
+
+export const DefaultFocused: Story = {
+  ...Default,
+  parameters: {
+    pseudo: {
+      focus: true,
+    },
+  },
+};
+
 export const Primary: Story = {
   args: {
     label: 'Log In',
   },
   render: (args) => ({
     props: args,
-    template: `<div class="filled-btn">${args['label']}</div>`,
+    template: `<button class="btn btn-primary">${args['label']}</button>`,
   }),
 };
 
-export const Hovered: Story = {
-  args: {
-    label: 'Log In',
-  },
+export const PrimaryHovered: Story = {
+  ...Primary,
   parameters: {
     pseudo: {
       hover: true,
     },
   },
-  render: (args) => ({
-    props: args,
-    template: `<div class="filled-btn">${args['label']}</div>`,
-  }),
 };
 
-export const Focused: Story = {
-  args: {
-    label: 'Log In',
-  },
+export const PrimaryFocused: Story = {
+  ...Primary,
   parameters: {
     pseudo: {
       focus: true,
     },
   },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Log In',
+  },
   render: (args) => ({
     props: args,
-    template: `<div class="filled-btn">${args['label']}</div>`,
+    template: `<button class="btn btn-secondary">${args['label']}</button>`,
   }),
+};
+
+export const SecondaryHovered: Story = {
+  ...Secondary,
+  parameters: {
+    pseudo: {
+      hover: true,
+    },
+  },
+};
+
+export const SecondaryFocused: Story = {
+  ...Secondary,
+  parameters: {
+    pseudo: {
+      focus: true,
+    },
+  },
 };
