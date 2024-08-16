396850ed925446fabadef2129735fe169028498c
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-16 19:01:07 +0200

    fix(storybook): update router import

--- src/stories/navigation.stories.ts
+++ src/stories/navigation.stories.ts
@@ -1,13 +1,13 @@
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { VerticalNavComponent } from '../app/shared/components/vertical-nav/vertical-nav.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 
 const meta: Meta = {
   title: 'Example/Navigation',
   component: VerticalNavComponent,
   decorators: [
     moduleMetadata({
-      imports: [RouterTestingModule],
+      imports: [provideRouter([])],
     }),
   ],
   tags: ['autodocs'],
