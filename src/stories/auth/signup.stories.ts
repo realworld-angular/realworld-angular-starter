a33a0bf378ab13bc902690570bd18769cefb3032
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-10-10 22:47:01 +0200

    refactor: remove unused provider

--- src/stories/auth/signup.stories.ts
+++ src/stories/auth/signup.stories.ts
@@ -1,15 +1,9 @@
-import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { Meta, StoryObj } from '@storybook/angular';
 import { SignupComponent } from '../../app/core/auth/signup/signup.component';
-import { AuthService } from '../../app/core/auth/auth.service';
 
 const meta: Meta = {
   title: 'Auth/Signup',
   component: SignupComponent,
-  decorators: [
-    moduleMetadata({
-      providers: [AuthService],
-    }),
-  ],
   tags: ['autodocs'],
   parameters: {
     layout: 'centered',
