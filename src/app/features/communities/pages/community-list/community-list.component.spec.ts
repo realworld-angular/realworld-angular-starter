7447a66a4e2897e5b610a29b12de9ecd0c971327
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-10-10 22:28:21 +0200

    fix: tests

--- src/app/features/communities/pages/community-list/community-list.component.spec.ts
+++ src/app/features/communities/pages/community-list/community-list.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CommunityListComponent } from './community-list.component';
+import CommunityListComponent from './community-list.component';
 
 describe('CommunityListComponent', () => {
   let component: CommunityListComponent;
