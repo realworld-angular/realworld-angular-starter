521292449a14531cc283c3f049b072ae70227bc9
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2025-01-17 02:21:35 +0100

    refactor: move to light theme

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  imports: [RouterLink, RouterLinkActive],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {}
