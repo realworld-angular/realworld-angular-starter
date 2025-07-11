f72bdb57841a7ddd0ffd2a63e43ffe1df52c0a4b
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2025-07-11 13:04:27 +0200

    refactor: angular 20 update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection, inject, provideAppInitializer } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideZonelessChangeDetection,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { Meta } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(),
-    provideAppInitializer(() =>  {
+    provideZonelessChangeDetection(),
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withFetch()),
+    provideAppInitializer(() => {
       inject(Meta).addTag({
         httpEquiv: 'Content-Security-Policy',
         content: environment.csp,
