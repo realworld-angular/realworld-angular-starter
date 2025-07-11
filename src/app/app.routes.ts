2d70e6dcc354f829e4861aed651243459fe21bc4
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2025-07-11 13:06:11 +0200

    refactor: remove features as part of changing the project purpose

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'communities',
-    loadChildren: () => import('./features/communities/communities.routes'),
-  },
-  {
-    path: 'events',
-    loadChildren: () => import('./features/events/events.routes'),
-  },
-  {
     path: 'about',
     loadChildren: () => import('./features/about/about.routes'),
   },
